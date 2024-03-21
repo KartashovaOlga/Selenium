73b2a966e2f3025c6f378ce3ad241598595d9add
OZabolotina <mcclenn@rambler.ru>
2024-03-21 12:27:16 +0300

    Add tests for task 6 and 7

---
+++ TestsForSeleniumTraining/src/test/java/org/example/AllLinksInMenuTest.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+public class AllLinksInMenuTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        System.setProperty("webdriver.chrome.driver", "D:\\DEVELOPMENT\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 11);
+    }
+
+    @Test
+    public void SimpleTest(){
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        List<WebElement> linksList = driver.findElements(By.xpath("//ul//li//a"));
+
+        int numberOfListElements = linksList.size();
+
+        for (int i = 0; i < numberOfListElements ; i++){
+            linksList = driver.findElements(By.cssSelector("#box-apps-menu a"));
+            linksList.get(i).click();
+            assertEquals(1, driver.findElements(By.xpath("//td[@id='content']//h1")).size());
+
+
+        }
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+
+}
---
+++ TestsForSeleniumTraining/src/test/java/org/example/StickersTest.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class StickersTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        System.setProperty("webdriver.chrome.driver", "D:\\DEVELOPMENT\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 11);
+    }
+
+    @Test
+    public void SimpleTest(){
+        driver.navigate().to("http://localhost/litecart");
+
+        List<WebElement> linksList = driver
+                .findElements(By.xpath("//ul[@class='listing-wrapper products']//li[@class='product column shadow hover-light']"));
+        List<WebElement> stickers;
+
+        for(WebElement element : linksList){
+
+            stickers  = element.findElements(By.cssSelector("[class ^=sticker]"));
+
+            assertEquals(1, stickers.size());
+        }
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+
+}
